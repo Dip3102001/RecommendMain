b5533d70eee87eabecbc594d47ecfc6bad396c20
Dip3102001 <dip3102001@gmail.com>
2025-06-26 10:58:03 -0400

    Created : model used in application

---
+++ model/Product.js
@@ -0,0 +1,35 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Product = sequelize.define('Product', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+  },
+  price: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false,
+  },
+  category: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  features: {
+    type: DataTypes.JSONB,
+    defaultValue: {},
+  },
+  isActive: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true,
+  },
+});
+
+module.exports = Product;

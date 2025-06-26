2fababf0b2cf36699faf579c4a21521208883863
Dip3102001 <dip3102001@gmail.com>
2025-06-26 10:51:21 -0400

    Added : configuration file to connect with DB

---
+++ config/qdrant.js
@@ -0,0 +1,20 @@
+const { QdrantClient } = require("@qdrant/js-client-rest");
+require("dotenv").config();
+
+// Initialize Qdrant client
+const qdrant = new QdrantClient({
+  url: process.env.QDRANT_URL,
+  apiKey: process.env.QDRANT_API_KEY,
+});
+
+const getCollection = async (collectionName = process.env.QDRANT_COLLECTION_NAME) => {
+  try {
+    const collectionInfo = await qdrant.getCollection(collectionName);
+    return collectionInfo;
+  } catch (error) {
+    console.error("Error connecting to Qdrant:", error);
+    throw error;
+  }
+};
+
+module.exports = { qdrant, getCollection };
